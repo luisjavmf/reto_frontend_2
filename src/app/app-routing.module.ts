5857b48281f7fdc230887dea253fc0b69e082038
Luis Javier Montes Fernandez <luis.montes@sophossolutions.com>
2023-05-05 15:41:27 -0500

    Modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
+import { TestsComponent } from './screens/tests/tests.component';
+import { AffiliatesComponent } from './screens/affiliates/affiliates.component';
+import { AppointmentsComponent } from './screens/appointments/appointments.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'tests', component: TestsComponent },
+  { path: 'affiliates', component: AffiliatesComponent },
+  { path: 'appointments', component: AppointmentsComponent },
 ];
 
 @NgModule({
