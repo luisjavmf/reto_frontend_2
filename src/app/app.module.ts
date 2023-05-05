5e62f1742fa68d11e7be59bed66a1c1678c4beb3
Luis Javier Montes Fernandez <luis.montes@sophossolutions.com>
2023-05-05 15:39:27 -0500

    Create Affiliates, Appointments and Tests components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './screens/navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './screens/home/home.component';
+import { AppointmentsComponent } from './screens/appointments/appointments.component';
+import { AffiliatesComponent } from './screens/affiliates/affiliates.component';
+import { TestsComponent } from './screens/tests/tests.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, AppointmentsComponent, AffiliatesComponent, TestsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
