617fc4a0a106921b15f5df9c1d312ece55f05b62
Luis Javier Montes Fernandez <luis.montes@sophossolutions.com>
2023-05-08 10:55:42 -0500

    Format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
-// import { MatMoment } from '@angular/material';
 
 import { HomeComponent } from './screens/home/home.component';
 import { AppointmentsComponent } from './screens/appointments/appointments.component';
