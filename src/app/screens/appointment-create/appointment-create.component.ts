22f9d68a6df8f40c1166fbcc7f76616b9be7b311
Luis Javier Montes Fernandez <luis.montes@sophossolutions.com>
2023-05-09 15:59:49 -0500

    Create Appointment-Create screen

---
+++ src/app/screens/appointment-create/appointment-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-create',
+  templateUrl: './appointment-create.component.html',
+  styleUrls: ['./appointment-create.component.css']
+})
+export class AppointmentCreateComponent {
+
+}
