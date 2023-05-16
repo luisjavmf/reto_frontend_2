681921d53ba3971c928b7441e37e1a8eaca48abf
Luis Javier Montes Fernandez <luis.montes@sophossolutions.com>
2023-05-16 16:41:24 -0500

    Create enviroments folder and add HttpClientModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -40,6 +41,7 @@ import { AppointmentCreateComponent } from './screens/appointment-create/appoint
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
